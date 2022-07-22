0eadb856216a47defc8a4ba61796048ceb348b7f
Craig Elliott <craigelliott84@gmail.com>
2022-07-22 15:24:42 +0200

    Complete logic
    Basic functionality complete. Have started OOP refactoring. No styling.

---
+++ board.py
@@ -0,0 +1,9 @@
+class Board:
+
+    def __init__(self):
+        self.row1 = ['1', '2', '3']
+        self.row2 = ['4', '5', '6']
+        self.row3 = ['7', '8', '9']
+        self.chosen_numbers = []
+        self.p1_nums = []
+        self.p2_nums = []
--- main.py
+++ main.py
@@ -1,27 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from board import Board
 
 app = Flask(__name__)
 Bootstrap(app)
 
-
-row1 = ['1', '2', '3']
-row2 = ['4', '5', '6']
-row3 = ['7', '8', '9']
-print(f"{row1}\n{row2}\n{row3}")
-
-
-@app.route("/")
-def home():
-    return render_template('index.html', row1=row1, row2=row2, row3=row3)
-
-
+board = Board()
 winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-chosen_numbers = []
-p1_nums = []
-p2_nums = []
 
 game_over = False
+player_one_move = True
+player_one_win = False
+player_two_win = False
+
+p1_score = 0
+p2_score = 0
+
+
+def start():
+    global board, game_over
+    board = Board()
+    game_over = False
+
+
+game_status = "Test"
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == 'POST':
+        start()
+    turn = request.args.get("turn")
+    global game_status, player_one_move, p1_score, p2_score
+    if player_one_move:
+        status('p1')
+    else:
+        status('p2')
+    if turn:
+        chosen_square = int(turn)
+        run_game(chosen_square)
+    return render_template('index.html',
+                           row1=board.row1,
+                           row2=board.row2,
+                           row3=board.row3,
+                           status=game_status,
+                           p1_score=p1_score,
+                           p2_score=p2_score)
+
+
+# winning_combos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+# chosen_numbers = []
+# p1_nums = []
+# p2_nums = []
 
 
 # This function checks the board for a win #
@@ -35,85 +65,115 @@ def check_for_win(player_nums):
         if score == 3:
             global game_over
             game_over = True
-            if player_nums == p1_nums:
+            if player_nums == board.p1_nums:
                 print("Player 1 wins! Xs 4 lyf")
+                status('p1win')
             else:
                 print("Player 2 wins! OOO m g!")
+                status('p2win')
+
+
+def run_game(chosen_square):
+    global player_one_move
+    if not game_over:
+        player_is_choosing = True
+        while player_is_choosing:
+            p_move = chosen_square
+            if player_one_move:
+                current_player = 'P1'
+                status('p2')
+                if not is_chosen(p_move):
+                    player_is_choosing = False
+                    add_to_board(p_move, current_player)
+                    check_for_win(board.p1_nums)
+                    player_one_move = False
+                else:
+                    player_is_choosing = False
+            else:
+                current_player = 'P2'
+                status('p1')
+                if not is_chosen(p_move):
+                    player_is_choosing = False
+                    add_to_board(p_move, current_player)
+                    check_for_win(board.p2_nums)
+                    player_one_move = True
+                else:
+                    player_is_choosing = False
+    else:
+        status('game_over')
 
 
 # Game loop #
 # Takes players' inputs and executes the above functions until a win is detected #
-def run_game():
-    while not game_over:
-        # player_move('player1')
-        player_is_choosing = True
-        while player_is_choosing:
-            p1_move = int(input("Player 1, pick a number: "))
-            current_player = 'Player 1'
-            if not is_chosen(p1_move):
-                player_is_choosing = False
-                add_to_board(p1_move, current_player)
-                check_for_win(p1_nums)
-
-        if not game_over:
-            # player_move('player2')
-            player_is_choosing = True
-            while player_is_choosing:
-                p2_move = int(input("Player 2, pick a number: "))
-                current_player = 'Player 2'
-                if not is_chosen(p2_move):
-                    player_is_choosing = False
-                    add_to_board(p2_move, current_player)
-                    check_for_win(p2_nums)
+# def run_game_console_version():
+#     while not game_over:
+#         # player_move('player1')
+#         player_is_choosing = True
+#         while player_is_choosing:
+#             p1_move = int(input("Player 1, pick a number: "))
+#             current_player = 'Player 1'
+#             if not is_chosen(p1_move):
+#                 player_is_choosing = False
+#                 add_to_board(p1_move, current_player)
+#                 check_for_win(p1_nums)
+#
+#         if not game_over:
+#             # player_move('player2')
+#             player_is_choosing = True
+#             while player_is_choosing:
+#                 p2_move = int(input("Player 2, pick a number: "))
+#                 current_player = 'Player 2'
+#                 if not is_chosen(p2_move):
+#                     player_is_choosing = False
+#                     add_to_board(p2_move, current_player)
+#                     check_for_win(p2_nums)
 
 
 # This function applies the player's symbol to the chosen spot on the board #
 def add_to_board(player_move, current_player):
-    # if player_move == p1_move:
-    if current_player == 'Player 1':
-        p1_nums.append(player_move)
+    if current_player == 'P1':
+        board.p1_nums.append(player_move)
         symbol = 'X'
     else:
-        p2_nums.append(player_move)
+        board.p2_nums.append(player_move)
         symbol = 'O'
     if player_move < 4:
-        row1[player_move - 1] = symbol
+        board.row1[player_move - 1] = symbol
     elif player_move < 7:
-        row2[player_move - 4] = symbol
+        board.row2[player_move - 4] = symbol
     else:
-        row3[player_move - 7] = symbol
-    print(f"{row1}\n{row2}\n{row3}")
+        board.row3[player_move - 7] = symbol
+    print(f"{board.row1}\n{board.row2}\n{board.row3}")
 
 
 # Checks if the chosen number has already been selected #
 def is_chosen(player_move):
-    if player_move in chosen_numbers:
-        print("You wish. Already taken. Pick again bey.")
+    if player_move in board.chosen_numbers:
+        # print("You wish. Already taken. Pick again bey.")
+        status('pick_again')
         return True
     else:
-        chosen_numbers.append(player_move)
+        board.chosen_numbers.append(player_move)
         return False
 
 
-# NOTE TO SELF: trying to get this function to replace the repeated code in the Game Loop below.
-# Not sure if it's actually less readable though. #
-# def player_move(player):
-#     player_is_choosing = True
-#     while player_is_choosing:
-#         if player == 'player1':
-#             p_nums = p1_nums
-#             p1_move = int(input("Player 1, pick a number: "))
-#             p_move = p1_move
-#         else:
-#             p_nums = p2_nums
-#             p2_move = int(input("Player 2, pick a number: "))
-#         if not is_chosen(p_move):
-#             player_is_choosing = False
-#             add_to_board(p_move)
-#             check_for_win(p_nums)
+def status(current_status):
+    global game_status, p1_score, p2_score
+    if current_status == 'p1win':
+        game_status = 'We have a winner! Xs 4 lyf!'
+        p1_score += 1
+    elif current_status == 'p2win':
+        game_status = 'Winner! Team O, bad boy!'
+        p2_score += 1
+    elif current_status == 'pick_again':
+        game_status = 'You wish. Already taken. Pick again bey.'
+    elif current_status == 'p1':
+        game_status = 'Team X, hit it'
+    elif current_status == 'p2':
+        game_status = 'Team O, get loose'
+    elif current_status == 'game_over':
+        game_status = 'Brah, the game is over. Start again dummy.'
 
-
-# run_game()
 
 if __name__ == "__main__":
     app.run(debug=True)
